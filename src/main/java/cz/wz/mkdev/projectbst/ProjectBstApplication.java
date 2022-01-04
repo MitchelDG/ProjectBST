19014d2c0ef585175abc58777ccd8e2f65381f80
Michal Kober <borek.michla@email.cz>
2022-01-04 19:07:43 +0100

    Initial commit

---
+++ src/main/java/cz/wz/mkdev/projectbst/ProjectBstApplication.java
@@ -0,0 +1,13 @@
+package cz.wz.mkdev.projectbst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/wz/mkdev/projectbst/ProjectBstApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.wz.mkdev.projectbst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
